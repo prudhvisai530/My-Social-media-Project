a730108c2217c50dbea82c5f8594cd965a1c47eb
prudhvisai530 <113229460+prudhvisai530@users.noreply.github.com>
2024-10-15 07:08:17 -0700

    Developed top bar of the application (#8)

--- webapp/social-media/src/components/topbar/Topbar.jsx
+++ webapp/social-media/src/components/topbar/Topbar.jsx
@@ -1,5 +1,42 @@
 import "./topbar.css";
+import { Search, Person, Chat, Notifications } from "@mui/icons-material";
 
 export default function Topbar() {
-  return <div>Topbar</div>;
+  return (
+    <div className="topBarContainer">
+      <div className="topBarLeft">
+        <span className="logo">Connect</span>
+      </div>
+      <div className="topBarCenter">
+        <div className="searchBar">
+          <Search className="searchIcon"></Search>
+          <input
+            placeholder="Search for People,Posr and video"
+            className="searchInput"
+          />
+        </div>
+      </div>
+      <div className="topBarRight">
+        <div className="topBarLinks">
+          <span className="topBarLink">Home</span>
+          <span className="topBarLink">Timeline</span>
+        </div>
+        <div className="topBarIcons">
+          <div className="topBarIconItem">
+            <Person className="topBarIcon"></Person>
+            <span className="topBarBadge">1</span>
+          </div>
+          <div className="topBarIconItem">
+            <Chat className="topBarIcon"></Chat>
+            <span className="topBarBadge">1</span>
+          </div>
+          <div className="topBarIconItem">
+            <Notifications className="topBarIcon"></Notifications>
+            <span className="topBarBadge">1</span>
+          </div>
+        </div>
+        <img src="/assets/person/1.jpeg" alt="" className="topBarImg" />
+      </div>
+    </div>
+  );
 }
