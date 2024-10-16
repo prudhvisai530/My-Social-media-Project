416222f737cc0a957089f67e3780344037fa9c64
prudhvisai530 <prudhvirock9@gmail.com>
2024-10-15 22:25:51 -0700

    body components commited

---
+++ webapp/social-media/src/components/feed/Feed.jsx
@@ -0,0 +1,5 @@
+import "./feed.css";
+
+export default function feed() {
+  return <div className="feed">feed</div>;
+}
---
+++ webapp/social-media/src/components/leftbar/Leftbar.jsx
@@ -0,0 +1,5 @@
+import "./leftbar.css";
+
+export default function Leftbar() {
+  return <div className="leftBar">Left Bar</div>;
+}
